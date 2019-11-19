4a2f74d6ff45cb8ef5ad7c472902d97a259f2c03
junfel <junfel@adarnadigital.com>
2019-11-19 16:17:22 +0800

    updated views things

--- sparta_pages/views.py
+++ sparta_pages/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from opaque_keys.edx.keys import CourseKey
 
+from .models import Pathway, SpartaCourse
+
 
 def main(request):
     """"""
     template_name = "sparta_main.html"
-    context = {"pathways": Pathway.objects.filter(is_active=True)}
+    context = {}
+
+    pathways = Pathway.objects.filter(is_active=True)
+
+    context['pathways'] = pathways
+
     return render(request, template_name, context)
 
 
