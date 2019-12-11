3b070ed124d78929d8bcebafd0227479c28f9fff
junfel <junfel@adarnadigital.com>
2019-12-11 17:18:37 +0800

    added register urls

--- sparta_pages/urls.py
+++ sparta_pages/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     url(r'^sparta/$', views.main, name='sparta-main'),
-    url(r'^sparta/register/$', views.registration_page, name='sparta-register'),
     url(r'^sparta/(?P<slug>[-\w]+)/$', views.pathway, name='sparta-pathway'),
     url(r'^sparta/upload/params/$', views.get_upload_params, name='sparta-upload-params'),
+    url(r'^sparta/register/demo/$', views.demo_registration_page, name='sparta-register-demo'),
+    url(r'^sparta/register/$', views.registration_page, name='sparta-register'),
+    url(r'^sparta/register/success/$', views.registration_page, name='sparta-register-success'),
 ]
