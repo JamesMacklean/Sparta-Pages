88b377dc2839dba702ffb406e36e26312e961018
junfel <junfel@adarnadigital.com>
2019-12-11 15:17:33 +0800

    added sparta models

--- sparta_pages/models.py
+++ sparta_pages/models.py
@@ -53,153 +53,151 @@ class SpartaCourse(models.Model):
     def __str__(self):
         return self.course_id
 
-#     @property
-#     def related_courses(self):
-#         pass
-#
-#
-# class SpartaProfile(models.Model):
-#     """
-#     """
-#
-#     user = models.OneToOneField(
-#         USER_MODEL,
-#         on_delete=models.CASCADE,
-#         help_text='User referred with this SPARTA User',
-#         related_name='sparta_profile')
-#     proof_of_identity = models.URLField(max_length=255)
-#     proof_of_education = models.URLField(max_length=255)
-#     proof_of_agreement = models.URLField(max_length=255)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     modified_at = models.DateTimeField(auto_now=True)
-#     first_timer = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=True)
-#
-#
-#     @property
-#     def pathwaysapproved(self):
-#         pass
-#
-#     @property
-#     def coursesapplied(self):
-#         pass
-#
-#     @property
-#     def enrollmentcodes(self):
-#         pass
-#
-#     @property
-#     def coursesenrolled(self):
-#         pass
-#
-#
-# class PathwayApplication(models.Model):
-#     """
-#     """
-#     PENDING = "PE"
-#     APPROVED = "AP"
-#     DENIED = "DE"
-#     WITHDRAWN = "WE"
-#     STATUS_CHOICES = (
-#         (PENDING, "Pending"),
-#         (APPROVED, "Approved"),
-#         (DENIED, "Denied"),
-#         (WITHDRAWN, "Withdrawn")
-#     )
-#     pathway = models.ForeignKey(
-#         'Pathway',
-#         on_delete=models.CASCADE,
-#         related_name="courses"
-#         )
-#     profile = models.ForeignKey(
-#         'SpartaProfile',
-#         on_delete=models.CASCADE,
-#         related_name="applications",
-#         verbose_name="sparta profile"
-#         )
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=PENDING)
-#
-#
-# class EducationProfile(models.Model):
-#     """
-#     """
-#     SECONDARY = "SE"
-#     ASSOCIATE = "AS"
-#     BACHELOR = "BA"
-#     MASTER = "MA"
-#     DOCTORAL = "DO"
-#     DEGREE_CHOICES = (
-#         (SECONDARY, "Secondary Education"),
-#         (ASSOCIATE, "Associate Degree"),
-#         (BACHELOR, "Bachelor's Degree"),
-#         (MASTER, "Master's Degree"),
-#         (DOCTORAL, "Doctoral Degree"),
-#     )
-#     profile = models.ForeignKey(
-#         'SpartaProfile',
-#         on_delete=models.CASCADE,
-#         related_name="education_profiles",
-#         verbose_name="sparta profile"
-#         )
-#     degree = models.CharField(max_length=2, choices=DEGREE_CHOICES, null=True, blank=True)
-#     course = models.CharField(max_length=255)
-#     school = models.CharField(max_length=255)
-#     address = models.TextField()
-#     started_at = models.DateField()
-#     graduated_at = models.DateField()
-#
-#
-# class EmploymentProfile(models.Model):
-#     """
-#     """
-#     PRIVATE = "PR"
-#     GOVERNMENT = "GO"
-#     ACADEME = "AC"
-#     AFF_CHOICES = (
-#         (PRIVATE, "Private"),
-#         (GOVERNMENT, "Government"),
-#         (ACADEME, "Academe"),
-#     )
-#     profile = models.ForeignKey(
-#         'SpartaProfile',
-#         on_delete=models.CASCADE,
-#         related_name="employment_profiles",
-#         verbose_name="sparta profile"
-#         )
-#     affiliation = models.CharField(max_length=2, choices=AFF_CHOICES, null=True, blank=True)
-#     occupation = models.CharField(max_length=255)
-#     designation = models.CharField(max_length=255)
-#     employer = models.CharField(max_length=255)
-#     address = models.TextField()
-#     started_at = models.DateField()
-#     ended_at = models.DateField()
-#
-#
-# class TrainingProfile(models.Model):
-#     """
-#     """
-#     profile = models.ForeignKey(
-#         'SpartaProfile',
-#         on_delete=models.CASCADE,
-#         related_name="training_profiles",
-#         verbose_name="sparta profile"
-#         )
-#     title = models.CharField(max_length=255)
-#     organizer = models.CharField(max_length=255)
-#     address = models.TextField()
-#     started_at = models.DateField()
-#     ended_at = models.DateField()
-#
-#
-# class Event(models.Model):
-#     """
-#     """
-#     event = models.CharField(max_length=255)
-#     description = models.CharField(max_length=255)
-#     profile = models.ForeignKey(
-#         'SpartaProfile',
-#         on_delete=models.CASCADE,
-#         verbose_name="sparta profile"
-#         )
-#     created_at = models.DateTimeField(auto_now_add=True)
+    @property
+    def related_courses(self):
+        pass
+
+
+class SpartaProfile(models.Model):
+    """
+    """
+    user = models.OneToOneField(
+        USER_MODEL,
+        on_delete=models.CASCADE,
+        help_text='User referred with this SPARTA User',
+        related_name='sparta_profile')
+    proof_of_education = models.URLField(max_length=255)
+    proof_of_agreement = models.URLField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    first_timer = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+
+    @property
+    def pathwaysapproved(self):
+        pass
+
+    @property
+    def coursesapplied(self):
+        pass
+
+    @property
+    def enrollmentcodes(self):
+        pass
+
+    @property
+    def coursesenrolled(self):
+        pass
+
+
+class PathwayApplication(models.Model):
+    """
+    """
+    PENDING = "PE"
+    APPROVED = "AP"
+    DENIED = "DE"
+    WITHDRAWN = "WE"
+    STATUS_CHOICES = (
+        (PENDING, "Pending"),
+        (APPROVED, "Approved"),
+        (DENIED, "Denied"),
+        (WITHDRAWN, "Withdrawn")
+    )
+    pathway = models.ForeignKey(
+        'Pathway',
+        on_delete=models.CASCADE,
+        related_name="courses"
+        )
+    profile = models.ForeignKey(
+        'SpartaProfile',
+        on_delete=models.CASCADE,
+        related_name="applications",
+        verbose_name="sparta profile"
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=PENDING)
+
+
+class EducationProfile(models.Model):
+    """
+    """
+    SECONDARY = "SE"
+    ASSOCIATE = "AS"
+    BACHELOR = "BA"
+    MASTER = "MA"
+    DOCTORAL = "DO"
+    DEGREE_CHOICES = (
+        (SECONDARY, "Secondary Education"),
+        (ASSOCIATE, "Associate Degree"),
+        (BACHELOR, "Bachelor's Degree"),
+        (MASTER, "Master's Degree"),
+        (DOCTORAL, "Doctoral Degree"),
+    )
+    profile = models.ForeignKey(
+        'SpartaProfile',
+        on_delete=models.CASCADE,
+        related_name="education_profiles",
+        verbose_name="sparta profile"
+        )
+    degree = models.CharField(max_length=2, choices=DEGREE_CHOICES, null=True, blank=True)
+    course = models.CharField(max_length=255)
+    school = models.CharField(max_length=255)
+    address = models.TextField()
+    started_at = models.DateField()
+    graduated_at = models.DateField()
+
+
+class EmploymentProfile(models.Model):
+    """
+    """
+    PRIVATE = "PR"
+    GOVERNMENT = "GO"
+    ACADEME = "AC"
+    AFF_CHOICES = (
+        (PRIVATE, "Private"),
+        (GOVERNMENT, "Government"),
+        (ACADEME, "Academe"),
+    )
+    profile = models.ForeignKey(
+        'SpartaProfile',
+        on_delete=models.CASCADE,
+        related_name="employment_profiles",
+        verbose_name="sparta profile"
+        )
+    affiliation = models.CharField(max_length=2, choices=AFF_CHOICES, null=True, blank=True)
+    occupation = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    employer = models.CharField(max_length=255)
+    address = models.TextField()
+    started_at = models.DateField()
+    ended_at = models.DateField()
+
+
+class TrainingProfile(models.Model):
+    """
+    """
+    profile = models.ForeignKey(
+        'SpartaProfile',
+        on_delete=models.CASCADE,
+        related_name="training_profiles",
+        verbose_name="sparta profile"
+        )
+    title = models.CharField(max_length=255)
+    organizer = models.CharField(max_length=255)
+    address = models.TextField()
+    started_at = models.DateField()
+    ended_at = models.DateField()
+
+
+class Event(models.Model):
+    """
+    """
+    event = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    profile = models.ForeignKey(
+        'SpartaProfile',
+        on_delete=models.CASCADE,
+        verbose_name="sparta profile"
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
