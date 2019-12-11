012af38c6e3e8e9ea27ff3b10195d665ba53d6ea
junfel <junfel@adarnadigital.com>
2019-12-11 15:17:57 +0800

    updated views

--- sparta_pages/urls.py
+++ sparta_pages/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^sparta/$', views.main, name='sparta-main'),
+    url(r'^sparta/register/$', views.register, name='sparta-main'),
     url(r'^sparta/(?P<slug>[-\w]+)/$', views.pathway, name='sparta-pathway'),
 ]
--- sparta_pages/views.py
+++ sparta_pages/views.py
@@ -1,11 +1,22 @@
+from base64 import b64encode
+from datetime import datetime, timedelta
+from json import dumps
+from mimetypes import guess_type
+from os import path
+from uuid import uuid4
+import hmac
+import sha
+
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from opaque_keys.edx.keys import CourseKey
 
+from .forms import SpartaProfileForm, EducationProfileForm, EmploymentProfileForm, TrainingProfileForm, PathwayApplicationForm
 from .models import Pathway, SpartaCourse
 
 
@@ -47,84 +58,104 @@ def pathway(request, slug):
     return render(request, template_name, context)
 
 
-# def registration_page(request):
-#     """ /sparta/register/ """
-#     template_name = "sparta_register.html"
-#     context = {}
-#
-#     return render(request, template_name, context)
-#
-#
-# @require_POST()
-# def register(request):
-#     """ /sparta/register/submit/ """
-#
-#     return redirect('sparta-register-success')
-#
-#
-# def register_success_page(request):
-#     """ /sparta/register/success/ """
-#     template_name = "sparta_register_success.html"
-#     context = {}
-#
-#     return render(request, template_name, context)
-#
-#
-# def sparta_profile_page(request, profile_id):
-#     """ /sparta/profile/{profile_id}/ """
-#     template_name = "sparta_profile.html"
-#     context = {}
-#
-#     return render(request, template_name, context)
-#
-#
-# def application_page(request, profile_id):
-#     """ /sparta/profile/{profile_id}/apply/ """
-#     template_name = "sparta_apply.html"
-#     context = {}
-#
-#     return render(request, template_name, context)
-#
-#
-# @require_POST()
-# def apply(request, profile_id, pathway_id):
-#     """ /sparta/profile/{profile_id}/apply/{pathway_id}/ """
-#
-#     return redirect('sparta-profile', profile_id=profile_id)
-#
-#
-# ###############
-# # Admin Pages #
-# ###############
-#
-# @login_required
-# def admin_main_view(request):
-#     if not request.user.is_staff:
-#         raise Http404
-#
-#     template_name = "sparta_admin.html"
-#     context = {}
-#
-#     return render(request, template_name, context)
-#
-#
-# @login_required
-# def admin_profiles_view(request):
-#     if not request.user.is_staff:
-#         raise Http404
-#
-#     template_name = "sparta_admin_profiles.html"
-#     context = {}
-#
-#     return render(request, template_name, context)
-#
-#
-# @login_required
-# def admin_applications_view(request):
-#     if not request.user.is_staff:
-#         raise Http404
-#
-#     template_name = "sparta_admin_applications.html"
-#     context = {}
-#
-#     return render(request, template_name, context)
+def registration_page(request):
+    """ /sparta/register/ """
+    template_name = "sparta_register.html"
+    context = {
+    'AWS_S3_BUCKET_URL': "http://{}.s3.amazonaws.com/".format(FILE_UPLOAD_STORAGE_BUCKET_NAME),
+    'AWS_ACCESS_KEY_ID': settings.AWS_ACCESS_KEY_ID
+    }
+    return render(request, template_name, context)
+
+
+def register_success_page(request):
+    """ /sparta/register/success/ """
+    template_name = "sparta_register_success.html"
+    context = {}
+
+    return render(request, template_name, context)
+
+
+def sparta_profile_page(request, profile_id):
+    """ /sparta/profile/{profile_id}/ """
+    template_name = "sparta_profile.html"
+    context = {}
+
+    return render(request, template_name, context)
+
+
+def application_page(request, profile_id):
+    """ /sparta/profile/{profile_id}/apply/ """
+    template_name = "sparta_apply.html"
+    context = {}
+
+    if request.method == 'POST':
+        form = PathwayApplicationForm(request.POST)
+        if form.is_valid():
+
+            return redirect('sparta-profile', profile_id=profile_id)
+    else:
+        form = PathwayApplicationForm()
+
+    return render(request, template_name, context)
+
+
+def get_upload_params(request):
+    def make_policy():
+        policy_object = {
+            "expiration": (datetime.now() + timedelta(hours=24)).strftime('%Y-%m-%dT%H:%M:%S.000Z'),
+            "conditions": [
+                { "bucket": settings.FILE_UPLOAD_STORAGE_BUCKET_NAME },
+                { "acl": "public-read" },
+                ["starts-with", "$key", "sparta_uploads/"],
+                { "success_action_status": "201" }
+            ]
+        }
+        return b64encode(dumps(policy_object).replace('\n', '').replace('\r', ''))
+
+    def sign_policy(policy):
+        return b64encode(hmac.new(settings.AWS_SECRET_ACCESS_KEY, policy, sha).digest())
+
+    policy = make_policy()
+    return HttpResponse(dumps({
+        "policy": policy,
+        "signature": sign_policy(policy),
+        "key": "sparta_uploads/" + uuid4().hex + ".bin",
+        "success_action_redirect": "/"
+    }), content_type="application/json")
+
+###############
+# Admin Pages #
+###############
+
+@login_required
+def admin_main_view(request):
+    if not request.user.is_staff:
+        raise Http404
+
+    template_name = "sparta_admin.html"
+    context = {}
+
+    return render(request, template_name, context)
+
+
+@login_required
+def admin_profiles_view(request):
+    if not request.user.is_staff:
+        raise Http404
+
+    template_name = "sparta_admin_profiles.html"
+    context = {}
+
+    return render(request, template_name, context)
+
+
+@login_required
+def admin_applications_view(request):
+    if not request.user.is_staff:
+        raise Http404
+
+    template_name = "sparta_admin_applications.html"
+    context = {}
+
+    return render(request, template_name, context)
