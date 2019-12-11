54e990a9a5eb85acedbfee7e65341100393725d4
junfel <junfel@adarnadigital.com>
2019-12-11 18:20:57 +0800

    updated urls for checking

--- sparta_pages/urls.py
+++ sparta_pages/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^sparta/$', views.main, name='sparta-main'),
     url(r'^sparta/(?P<slug>[-\w]+)/$', views.pathway, name='sparta-pathway'),
     url(r'^sparta/upload/params/$', views.get_upload_params, name='sparta-upload-params'),
-    url(r'^sparta/register/demo/$', views.demo_registration_page, name='sparta-register-demo'),
+    # url(r'^sparta/register/demo/$', views.demo_registration_page, name='sparta-register-demo'),
     url(r'^sparta/register/$', views.registration_page, name='sparta-register'),
     url(r'^sparta/register/success/$', views.register_success_page, name='sparta-register-success'),
 ]
