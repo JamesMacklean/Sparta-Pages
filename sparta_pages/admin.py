51c4bfb3a3fe6fba6c2d49cc62326b85bffd3eb3
junfel <junfel@adarnadigital.com>
2019-12-11 15:49:15 +0800

    updated admin views

--- sparta_pages/admin.py
+++ sparta_pages/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Pathway, SpartaCourse
+from .models import Pathway, SpartaCourse, SpartaProfile, PathwayApplication, EducationProfile, EmploymentProfile, TrainingProfile, Event
 
 admin.site.register(Pathway)
 admin.site.register(SpartaCourse)
+admin.site.register(SpartaProfile)
+admin.site.register(PathwayApplication)
+admin.site.register(EducationProfile)
+admin.site.register(EmploymentProfile)
+admin.site.register(TrainingProfile)
+admin.site.register(Event)
