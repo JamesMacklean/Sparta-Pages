105516f88b40f236414aac668bc43eff3ec01e0f
junfel <junfel@adarnadigital.com>
2019-12-12 17:39:50 +0800

    added utils

---
+++ sparta_pages/utils.py
@@ -0,0 +1,10 @@
+from opaque_keys.edx.keys import CourseKey
+from student.models import CourseEnrollment
+
+from .models import Pathway, SpartaCourse, SpartaProfile, EducationProfile, EmploymentProfile, TrainingProfile, PathwayApplication, Event
+
+
+def manage_pathway_applications():
+    """"""
+    for app in PathwayApplication.objects.filter(status="PE"):
+        app.approve()
