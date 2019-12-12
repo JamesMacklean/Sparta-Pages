63d1ecba383af9e89da645735aba331f9f8aa474
junfel <junfel@adarnadigital.com>
2019-12-12 10:33:16 +0800

    fixed with adding missing imports

--- sparta_pages/views.py
+++ sparta_pages/views.py
@@ -16,6 +16,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.http import require_POST
 
