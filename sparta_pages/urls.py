ca26f51e2652129ba2ae51146a095cf81549a62f
junfel <junfel@adarnadigital.com>
2019-12-12 10:31:30 +0800

    changed order to fix bug when trying to get non pathway pages

--- sparta_pages/urls.py
+++ sparta_pages/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^sparta/$', views.main, name='sparta-main'),
-    url(r'^sparta/(?P<slug>[-\w]+)/$', views.pathway, name='sparta-pathway'),
-    url(r'^sparta/upload/params/$', views.get_upload_params, name='sparta-upload-params'),
     url(r'^sparta/register/$', views.RegistrationPageView.as_view(), name='sparta-register'),
+    url(r'^sparta/upload/params/$', views.get_upload_params, name='sparta-upload-params'),
+    url(r'^sparta/(?P<slug>[-\w]+)/$', views.pathway, name='sparta-pathway'),
 ]
