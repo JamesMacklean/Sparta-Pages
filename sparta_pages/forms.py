a64b2e9c100255f2c923fc5154e1bd81027cf84c
junfel <junfel@adarnadigital.com>
2019-12-13 17:15:20 +0800

    updated forms datefield

--- sparta_pages/forms.py
+++ sparta_pages/forms.py
@@ -21,32 +21,61 @@ class SpartaProfileForm(forms.Form):
 class EducationProfileForm(forms.ModelForm):
     """
     """
+    started_at = forms.DateField(
+        widget=forms.SelectDateWidget(
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+            ),
+        )
+    graduated_at = forms.DateField(
+        widget=forms.SelectDateWidget(
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+            ),
+        )
     class Meta:
         model = EducationProfile
         fields = [
-            'degree', 'course', 'school', 'address', 'started_at', 'graduated_at'
+            'degree', 'course', 'school', 'address',
         ]
 
 
 class EmploymentProfileForm(forms.ModelForm):
     """
     """
+    started_at = forms.DateField(
+        widget=forms.SelectDateWidget(
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+            ),
+        )
+    ended_at = forms.DateField(
+        widget=forms.SelectDateWidget(
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+            ),
+        )
     class Meta:
         model = EmploymentProfile
         fields = [
             'affiliation', 'occupation', 'designation',
-            'employer', 'address', 'started_at', 'ended_at'
+            'employer', 'address',
         ]
 
 
 class TrainingProfileForm(forms.ModelForm):
     """
     """
+    started_at = forms.DateField(
+        widget=forms.SelectDateWidget(
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+            ),
+        )
+    ended_at = forms.DateField(
+        widget=forms.SelectDateWidget(
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+            ),
+        )
     class Meta:
         model = TrainingProfile
         fields = [
             'title', 'organizer', 'address',
-            'started_at', 'ended_at'
         ]
 
 
