77cbcf19da80868cb8861b98aed9e1d60bc3d4d0
junfel <junfel@adarnadigital.com>
2019-12-18 10:45:12 +0800

    update admin view for proper query of apps

--- sparta_pages/views.py
+++ sparta_pages/views.py
@@ -709,18 +709,29 @@ def admin_applications_view(request):
     template_name = "sparta_admin_applications.html"
     context = {}
 
-    applications = PathwayApplication.objects.all()
+    date_today = date.today()
+    buffer_date = date_today - timedelta(days=3)
+    applications = PathwayApplication.objects.all().filter(created_at__gte=buffer_date).filter(created_at__lte=)
 
-    date_from = request.GET.get('date_from', None)
-    date_to = request.GET.get('date_to', None)
+    date_from_year = request.GET.get('date_from_year', None)
+    date_from_month = request.GET.get('date_from_month', None)
+    date_from_day = request.GET.get('date_from_day', None)
+    if None not in [date_from_year, date_from_month, date_from_day]:
+        date_from = "{}-{}-{}".format(date_from_year, date_from_month, date_from_day)
+
+    date_to_year = request.GET.get('date_to_year', None)
+    date_to_month = request.GET.get('date_to_month', None)
+    date_to_day = request.GET.get('date_to_day', None)
+    if None not in [date_to_year, date_to_month, date_to_day]:
+        date_to = "{}-{}-{}".format(date_to_year, date_to_month, date_to_day)
 
     if date_from or date_to:
         if not date_from:
-            date_from = date.today()
+            date_from = date_today
         else:
             date_from = datetime.strptime(date_from, "%Y-%m-%d").date()
         if not date_to:
-            date_to = date.today()
+            date_to = date_today
         else:
             date_to = datetime.strptime(date_to, "%Y-%m-%d").date()
         applications.filter(created_at__gte=date_from).filter(created_at__lte=date_to)
