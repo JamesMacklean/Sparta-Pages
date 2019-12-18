018f712d916c00d03fe91e556fcd0d8e61a7360f
junfel <junfel@adarnadigital.com>
2019-12-18 14:23:31 +0800

    fixed email message with time interval content

--- sparta_pages/utils.py
+++ sparta_pages/utils.py
@@ -18,11 +18,16 @@ def manage_pathway_applications():
 def manage_sparta_enrollments(date_from=None, date_to=None):
     applications = PathwayApplication.objects.all()
 
+    datefrom_str = ""
+    dateto_str = ""
+
     if date_from:
         applications = applications.filter(created_at__gte=date_from)
+        datefrom_str = datefrom.strftime('%Y-%m-%d')
 
     if date_to:
         applications = applications.filter(created_at__lte=date_to)
+        dateto_str = dateto.strftime('%Y-%m-%d')
 
     student_list = []
     for app in applications.filter(status="AP"):
@@ -30,6 +35,7 @@ def manage_sparta_enrollments(date_from=None, date_to=None):
         student_list.append({'user': app.profile.user, 'pathway': app.pathway, 'courses': app.pathway.courses.all()})
 
     tnow = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000Z')
+
     file_name = '/home/ubuntu/tempfiles/sparta_applications_file_{}.csv'.format(tnow)
     with open(file_name, mode='w') as apps_file:
         writer = csv.writer(apps_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -44,9 +50,14 @@ def manage_sparta_enrollments(date_from=None, date_to=None):
                 course_keys = course_keys + course.course_id + ";"
             writer.writerow([user.id, user.username, user.email, pathway.id, pathway.name, course_keys])
 
+    if datefrom_str or dateto_str:
+        date_range = ' for date range: {} to {}'.format(datefrom_str, dateto_str)
+    else:
+        date_range = ""
+
     email = EmailMessage(
         'Coursebank - SPARTA Applications - {}'.format(tnow),
-        'Attached file of SPARTA Applications - {}'.format(tnow),
+        'Attached file of SPARTA Applications{}'.format(date_range),
         'no-reply-sparta-applications@coursebank.ph',
         ['learn@coursebank.ph',],
     )
