6f50715c4e7531539a2cb850b98da7bf8207e808
junfel <junfel@adarnadigital.com>
2019-12-18 10:15:03 +0800

    updated admin views

--- sparta_pages/admin.py
+++ sparta_pages/admin.py
@@ -1,11 +1,53 @@
 from django.contrib import admin
 from .models import Pathway, SpartaCourse, SpartaProfile, PathwayApplication, EducationProfile, EmploymentProfile, TrainingProfile, Event
 
-admin.site.register(Pathway)
-admin.site.register(SpartaCourse)
-admin.site.register(SpartaProfile)
-admin.site.register(PathwayApplication)
-admin.site.register(EducationProfile)
-admin.site.register(EmploymentProfile)
-admin.site.register(TrainingProfile)
-admin.site.register(Event)
+
+@admin.register(Pathway)
+class PathwayAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SpartaCourse)
+class SpartaCourseAdmin(admin.ModelAdmin):
+    list_display = ('pathway', 'course_id')
+    list_filter = ('pathway',)
+    search_fields = ['pathway__name', 'course_id']
+
+
+@admin.register(SpartaProfile)
+class SpartaProfileAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'user__email']
+
+
+@admin.register(PathwayApplication)
+class PathwayApplicationAdmin(admin.ModelAdmin):
+    list_display = ('profile__user__username', 'pathway__name', 'status')
+    list_filter = ('pathway', 'status')
+    search_fields = ['pathway__name', 'profile__user__username', 'profile__user__email']
+
+
+@admin.register(EducationProfile)
+class EducationProfileAdmin(admin.ModelAdmin):
+    list_display = ('profile__user__username', 'course')
+    search_fields = ['profile__user__username', 'profile__user__email', 'degree', 'course', 'school', 'address']
+
+
+@admin.register(EmploymentProfile)
+class EmploymentProfileAdmin(admin.ModelAdmin):
+    list_display = ('profile__user__username', 'occupation')
+    search_fields = [
+        'profile__user__username', 'profile__user__email',
+        'affiliation', 'occupation', 'designation', 'employer', 'address'
+    ]
+
+
+@admin.register(TrainingProfile)
+class TrainingProfileAdmin(admin.ModelAdmin):
+    list_display = ('profile__user__username', 'title')
+    search_fields = ['profile__user__username', 'profile__user__email', 'title', 'organizer', 'address']
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('profile__user__username', 'event')
+    search_fields = ['profile__user__username', 'profile__user__email', 'event', 'description']
