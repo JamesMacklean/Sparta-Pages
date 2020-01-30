7caebf80e51c2fefb1da61b3b01e459dc824ef10
junfel <junfel@adarnadigital.com>
2020-01-30 17:27:45 +0800

    file: unfinished code for analytics

---
+++ sparta_pages/analytics.py
@@ -0,0 +1,261 @@
+
+
+############
+# OVERALL #
+###########
+
+def overall_no_of_enrollees():
+    """
+    Number of learners who enrolled as SPARTA scholars
+    """
+
+def overall_no_of_learners_in_progress():
+    """
+    Number of enrolled learners who are in-progress in at least 1 course
+    """
+
+def percent_of_learners_in_progress():
+    """
+    Percentage of enrollees who are in-progress in at least 1 course
+    """
+
+def overall_no_of_active_learners():
+    """
+    Number of learners who are in-progress in at least 1 course and whose last session is at most 30 days ago
+    """
+
+def percent_of_active_learners():
+    """
+    Percentage of learners who are in-progress in at least 1 course and whose last session is at most 30 days ago
+    """
+
+def overall_no_of_inactive_learners():
+    """
+    Number of learners who are in-progress in at least 1 course and whose last session is over 30 days ago but at most 180 days ago
+    """
+
+def percent_of_inactive_learners():
+    """
+    Percentage of learners who are in-progress in at least 1 course and whose last session is over 30 days ago but at most 180 days ago
+    """
+
+def overall_no_of_dropped_out_learners():
+    """
+    Number of learners who are in-progress in at least 1 course and whose last session is over 180 days ago
+    """
+
+def overall_dropout_rate():
+    """
+    Percentage of learners who are in-progress in at least 1 course and whose last session is over 180 days ago
+    """
+
+def overall_no_of_graduates():
+    """
+    Number of learners who completed at least 1 pathway
+    """
+
+def overall_graduation_rate():
+    """
+    Percentage of learners completed at least 1 pathway
+    """
+
+def overall_average_session_duration():
+    """
+    Total duration of all sessions divided by total number of sessions
+    """
+
+
+def overall_course_units_session():
+    """
+    Average number of course units viewed during a session in the Coursebank Studio
+    """
+
+
+#############
+# PATHWAYS #
+############
+
+def no_of_pathway_enrollees():
+    """
+    Number of learners who enrolled as SPARTA scholars (pathway-level)
+    """
+
+
+def no_of_pathway_learners_in_progress():
+    """
+    Number of enrolled learners who are in-progress in at least 1 course (pathway-level)
+    """
+
+
+def percent_of_pathway_learners_in_progress():
+    """
+    Percentage of enrollees who are in-progress in at least 1 course (pathway-level)
+    """
+
+
+def no_of_active_pathway_learners():
+    """
+    Number of learners who are in-progress in at least 1 course and whose last session is at most 30 days ago (pathway-level)
+    """
+
+
+def percent_of_active_pathway_learners():
+    """
+    Percentage of learners who are in-progress in at least 1 course and whose last session is at most 30 days ago (pathway-level)
+    """
+
+
+def no_of_inactive_pathway_learners():
+    """
+    Number of learners who are in-progress in at least 1 course and whose last session is over 30 days ago but at most 180 days ago (pathway-level)
+    """
+
+
+def percent_of_inactive_pathway_learners():
+    """
+    Percentage of learners who are in-progress in at least 1 course and whose last session is over 30 days ago but at most 180 days ago (pathway-level)
+    """
+
+
+def no_of_dropped_out_pathway_learners():
+    """
+    Number of learners who are in-progress in at least 1 course and whose last session is over 180 days ago (pathway-level)
+    """
+
+
+def pathway_dropout_rate():
+    """
+    Percentage of learners who are in-progress in at least 1 course and whose last session is over 180 days ago (pathway-level)
+    """
+
+
+def no_of_pathway_graduates():
+    """
+    Number of learners who completed the pathway
+    """
+
+
+def pathway_graduation_rate():
+    """
+    Percentage of learners completed the pathway
+    """
+
+
+def pathway_average_session_duration():
+    """
+    Total duration of all sessions divided by total number of sessions (pathway-level)
+    """
+
+
+def pathway_course_units_session():
+    """
+    Average number of course units viewed during a session in the Coursebank Studio(?) (pathway-level)
+    """
+
+
+###########
+# COURSE #
+##########
+
+
+def no_of_learners_in_progress():
+    """
+    Number of learners who are in-progress in the course
+    """
+
+
+def percent_of_learners_in_progress():
+    """
+    Percentage of enrollees who are in-progress in the course
+    """
+
+
+def no_of_active_learners():
+    """
+    Number of learners who are in-progress in the course and whose last session is at most 30 days ago
+    """
+
+
+def percent_of_active_learners():
+    """
+    Percentage of learners who are in-progress in the course and whose last session is at most 30 days ago
+    """
+
+
+def no_of_inactive_learners():
+    """
+    Number of learners who are in-progress in the course and whose last session is over 30 days ago but at most 180 days ago
+    """
+
+
+def percent_of_inactive_learners():
+    """
+    Percentage of learners who are in-progress in the course and whose last session is over 30 days ago but at most 180 days ago
+    """
+
+
+def no_of_dropped_out_learners():
+    """
+    Number of learners who are in-progress in the course and whose last session is over 180 days ago
+    """
+
+
+def dropout_rate():
+    """
+    Percentage of learners who are in-progress in the course and whose last session is over 180 days ago
+    """
+
+
+def no_of_completed_learners():
+    """
+    Number of learners who completed the course
+    """
+
+
+def completion_rate():
+    """
+    Percentage of learners who completed the course
+    """
+
+
+def average_session_duration():
+    """
+    Total duration of all sessions divided by total number of sessions (course-level)
+    """
+
+
+def course_units_session():
+    """
+    Average number of course units viewed during a session in the Coursebank Studio  (course-level)
+    """
+
+
+def net_promoter_score():
+    """
+    Net promoter score.  Computed as percentage of respondents who gave a rating of 8-10 minus the percentage of respondents who gave a rating of 1-3
+    """
+
+x = """
+    Course Objectives and Expectations Average Rating
+
+
+    Selection, Sequencing, and Organization Average Rating
+
+
+    Subject Matter Average Rating
+
+
+    Subject Matter Expert Average Rating
+
+
+    Course
+
+
+    Course Unit
+
+
+    Average Rating
+
+
+    Average Session Duration
+    """
