baa0ee9c4b64c75ba715fa5d21d40d73bd6a790e
junfel <junfel@adarnadigital.com>
2020-01-31 16:14:45 +0800

    file: added unfinished test code

---
+++ sparta_pages/tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class AnimalTestCase(TestCase):
+    """
+    """
+
+    def setUp(self):
+        """"""
+
+
+class Tests(APITestCase):
+    """
+    """
+
+    def setUp(self):
+        """"""
