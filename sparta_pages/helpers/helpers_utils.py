7cadd09c454677bd13c6891f19a4d44255ac2797
Junfel <junfel@adarnadigital.com>
2020-03-09 15:48:51 +0800

    rf: commented out logger

--- sparta_pages/helpers/helpers_utils.py
+++ sparta_pages/helpers/helpers_utils.py
@@ -106,7 +106,7 @@ def get_course_outline_block_tree(user, course_id):
 
                 completable_blocks = []
                 for child in block['children']:
-                    logger.info("recurse_mark_complete.child: {}".format(str(child)))
+                    # logger.info("recurse_mark_complete.child: {}".format(str(child)))
                     if 'type@discussion' not in child.get('id') and 'type@html' not in child.get('id'):
                         completable_blocks.append(child)
                 if len([child['complete'] for child in block['children']
