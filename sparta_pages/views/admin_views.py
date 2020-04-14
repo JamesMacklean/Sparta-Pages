7f3cb28184317581d7894b4727eaa89fdf92b1df
Junfel Tablac <junfel@buri.io>
2020-04-14 18:10:20 +0800

    rf: moved methods

--- sparta_pages/views/admin_views.py
+++ sparta_pages/views/admin_views.py
@@ -34,6 +34,16 @@ from ..models import (
     EducationProfile, EmploymentProfile, TrainingProfile,
     PathwayApplication, APIToken
 )
+from ..api.utils import (
+    get_sparta_course_id_list,
+    get_sparta_courses,
+    get_sparta_enrollees_by_class,
+    get_sparta_enrollees_by_age,
+    get_sparta_enrollees_by_gender,
+    get_sparta_enrollees_by_location,
+    weeklydates,
+    get_increase_in_enrollees
+)
 
 
 class DeveloperProfileView(TemplateView):
@@ -870,64 +880,6 @@ def data_dashboard_training_credentials_view(request):
     return render(request, template_name, context)
 
 
-def get_sparta_course_id_list():
-    course_id_list = []
-    for course in SpartaCourse.objects.filter(is_active=True):
-        if course.course_id not in course_id_list:
-            course_id_list.append(course.course_id)
-    return course_id_list
-
-
-def get_sparta_courses(course_id_list=None, course_enrollments=None):
-    if course_id_list is None:
-        course_id_list = get_sparta_course_id_list()
-    if course_enrollments is None:
-        course_enrollments = CourseEnrollment.objects.filter(is_active=True)
-
-    courses = []
-    for course_id in course_id_list:
-        course_key = CourseKey.from_string(course_id)
-        courseoverview = CourseOverview.get_from_id(course_key)
-
-        this_course_enrollments = course_enrollments.filter(course=courseoverview)
-        audit_enrollments = this_course_enrollments.filter(mode='audit')
-        verified_enrollments = this_course_enrollments.filter(mode='verified')
-
-        total_enrollments_count = this_course_enrollments.count()
-
-        cert_count = 0
-        for student in this_course_enrollments:
-            cert_status = certificate_status_for_student(student.user, course_key)
-            if cert_status and cert_status['mode'] == 'verified' or cert_status and cert_status['mode'] == 'honor':
-                if cert_status['status'] not in  ['unavailable', 'notpassing', 'restricted', 'unverified']:
-                    cert_count += 1
-
-        name = courseoverview.display_name
-        data = {
-            'course_id': course_id,
-            'slug': name.lower().replace(" ", "_").replace("-", "_"),
-            'name': name,
-            'total_no_of_enrollees': total_enrollments_count,
-            'no_of_completed': cert_count,
-            'no_of_unfinished': total_enrollments_count - cert_count,
-            'percent_completed': str(100*cert_count/total_enrollments_count) if total_enrollments_count > 0 else "0"
-        }
-
-        if audit_enrollments.exists():
-            data['no_of_audit_enrollees'] = audit_enrollments.count()
-        else:
-            data['no_of_audit_enrollees'] = 0
-
-        if verified_enrollments.exists():
-            data['no_of_verified_enrollees'] = verified_enrollments.count()
-        else:
-            data['no_of_verified_enrollees'] = 0
-
-        courses.append(data)
-
-    return courses
-
-
 @login_required
 def data_dashboard_courses_view(request):
     """"""
@@ -938,160 +890,6 @@ def data_dashboard_courses_view(request):
         "courses": get_sparta_courses(),
     }
     return render(request, template_name, context)
-
-
-def get_sparta_enrollees_by_class(profiles=None, extended_profiles=None):
-    if profiles is None:
-        profiles = SpartaProfile.objects.all()
-    if extended_profiles is None:
-        extended_profiles = ExtendedSpartaProfile.objects.all()
-
-    others_diff = profiles.count() - extended_profiles.count()
-
-    student_queryset = extended_profiles.filter(affiliation=ExtendedSpartaProfile.STUDENT)
-    student_count = student_queryset.count() if student_queryset.exists() else 0
-
-    faculty_queryset = extended_profiles.filter(affiliation=ExtendedSpartaProfile.FACULTY)
-    faculty_count = faculty_queryset.count() if faculty_queryset.exists() else 0
-
-    private_queryset = extended_profiles.filter(affiliation=ExtendedSpartaProfile.PRIVATE)
-    private_count = private_queryset.count() if private_queryset.exists() else 0
-
-    government_queryset = extended_profiles.filter(affiliation=ExtendedSpartaProfile.GOVERNMENT)
-    government_count = government_queryset.count() if government_queryset.exists() else 0
-
-    data = {
-        'student': student_count,
-        'faculty': faculty_count,
-        'private': private_count,
-        'government': government_count,
-        'others': others_diff if others_diff > 0 else 0
-    }
-    return data
-
-
-def get_sparta_enrollees_by_age(profiles=None):
-    if profiles is None:
-        profiles = SpartaProfile.objects.all()
-
-    this_year = datetime.now().year
-
-    data = {}
-    data['no_age'] = 0
-    for profile in profiles:
-        try:
-            year_of_birth = profile.user.profile.year_of_birth
-        except:
-            data['no_age'] += 1
-        else:
-            if year_of_birth is not None:
-                age = this_year - year_of_birth
-                if age not in data:
-                    data[str(age)] = 0
-                data[str(age)] += 1
-            else:
-                data['no_age'] += 1
-
-    list_data = []
-    for a in data:
-        list_data.append({
-            'age': str(a),
-            'count': data[a]
-        })
-
-    return list_data[::-1]
-
-
-def get_sparta_enrollees_by_gender(profiles=None):
-    if profiles is None:
-        profiles = SpartaProfile.objects.all()
-
-    male_count = 0
-    female_count = 0
-    other_count = 0
-    for profile in profiles:
-        try:
-            gender = profile.user.profile.gender
-        except:
-            other_count += 1
-        else:
-            if gender == 'm':
-                male_count += 1
-            elif gender == 'f':
-                female_count += 1
-            else:
-                other_count += 1
-    return {
-        'male': male_count,
-        'female': female_count,
-        'other': other_count
-    }
-
-
-def get_sparta_enrollees_by_location(profiles=None, extended_profiles=None):
-    if profiles is None:
-        profiles = SpartaProfile.objects.all()
-    if extended_profiles is None:
-        extended_profiles = ExtendedSpartaProfile.objects.all()
-
-    others_diff = profiles.count() - extended_profiles.count()
-
-    data = {}
-    for profile in extended_profiles:
-        municipality = profile.municipality
-        if municipality not in data:
-            data[municipality] = 0
-        data[municipality] += 1
-
-    list_data = []
-    for m in data:
-        if m == "---":
-            continue
-        list_data.append({
-            'city': m,
-            'count': data[m]
-        })
-    return sorted(list_data, key = lambda i: i['count'], reverse=True)[:10]
-
-
-def weeklydates(start_date, end_date, weekday=2):
-    d = start_date
-    d += timedelta(days = weekday - d.weekday())
-    while d <= end_date:
-        yield d
-        d += timedelta(days = 7)
-
-
-def get_increase_in_enrollees(profiles=None, course_id_list=None, course_enrollments=None, start_date=None):
-    if profiles is None:
-        profiles = SpartaProfile.objects.all()
-    if course_id_list is None:
-        course_id_list = get_sparta_course_id_list()
-    if course_enrollments is None:
-        course_enrollments = CourseEnrollment.objects.filter(is_active=True)
-
-    datetime_list = []
-    if start_date is None:
-        start_date = date(2020,1,1)
-    end_date = datetime.now().date()
-    for d in weeklydates(start_date, end_date):
-        datetime_list.append(d)
-
-    list_data = []
-    for d in datetime_list:
-        interval_enrollments = course_enrollments.filter(created__lte=d)
-        enrollment_counter = 0
-        for course_id in course_id_list:
-            course_key = CourseKey.from_string(course_id)
-            courseoverview = CourseOverview.get_from_id(course_key)
-            this_course_enrollments = interval_enrollments.filter(course=courseoverview)
-            enrollment_counter += this_course_enrollments.count()
-        list_data.append({
-            'date': d.strftime('%Y-%m-%d'),
-            'count': enrollment_counter
-        })
-
-    return list_data
 
 
 @login_required
@@ -1202,6 +1000,6 @@ def data_dashboard_graphs_by_date_view(request):
     course_enrollments = CourseEnrollment.objects.filter(is_active=True)
 
     context = {
-        "no_of_enrollees_by_date": get_increase_in_enrollees(profiles=profiles, course_id_list=course_id_list, course_enrollments=course_enrollments)
+        "no_of_enrollees_by_date": get_increase_in_enrollees(course_id_list=course_id_list, course_enrollments=course_enrollments)
     }
     return render(request, template_name, context)
