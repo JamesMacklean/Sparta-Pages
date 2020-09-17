a9c54fc52285d907dfed9fe6748c26b0744ee085
buri <junfel@buri.io>
2020-09-17 15:23:31 +0800

    feat: pathway list url

--- sparta_pages/api/urls.py
+++ sparta_pages/api/urls.py
@@ -42,6 +42,8 @@ urlpatterns = [
         analytics.pathway_application_detail, name='analytics-sparta-pathway-application-detail'),
     url(r'^sparta/api/v1/profiles/(?P<profile_id>\d+)/course/{}/grades'.format(settings.COURSE_ID_PATTERN), analytics.user_grade_detail,
         name='analytics-sparta-user-grade-detail'),
+    url(r'^sparta/api/v1/pathways$', analytics.pathway_list,
+        name='analytics-sparta-pathway-list'),
     url(r'^sparta/api/v1/courses$', analytics.course_list,
         name='analytics-sparta-course-list'),
 ]
