541783dcdedd83d9bdcde6bdba56478e2eac6168
elkin <elkin@coursebank.ph>
2020-10-23 11:29:34 +0800

    edit unenroll command

--- sparta_pages/management/commands/six_month_unenroll.py
+++ sparta_pages/management/commands/six_month_unenroll.py
@@ -41,7 +41,7 @@ class Command(BaseCommand):
             raise CommandError("Course does not exist: {}".format(str(e)))
 
         tnow = timezone.now()
-        date_filter = tnow - datetime.timedelta(days=180)
+        date_filter = tnow - datetime.timedelta(days=183)
 
         enrollments = CourseEnrollment.objects.filter(
             course_id=course_key,
