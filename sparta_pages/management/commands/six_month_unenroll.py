d101f33bbb66b1dcd239225dc6c442cedcd87f91
elkin <elkin@coursebank.ph>
2020-10-23 16:38:54 +0800

    edit unenroll command

--- sparta_pages/management/commands/six_month_unenroll.py
+++ sparta_pages/management/commands/six_month_unenroll.py
@@ -37,7 +37,7 @@ class Command(BaseCommand):
 
         try:
             course_key = CourseKey.from_string(course_id)
-            self.stdout.write("course_key: {}".format(courer_key))
+            self.stdout.write("course_key: {}".format(course_key))
         except Exception as e:
             raise CommandError("Course does not exist: {}".format(str(e)))
 
