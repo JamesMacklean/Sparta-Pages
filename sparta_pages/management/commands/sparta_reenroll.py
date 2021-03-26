8f59f9d617a818e69e31ccf09b8cc743e3e7141c
elkin <elkin@coursebank.ph>
2021-03-26 11:12:03 +0800

    edit sparta_reenroll command

--- sparta_pages/management/commands/sparta_reenroll.py
+++ sparta_pages/management/commands/sparta_reenroll.py
@@ -1,11 +1,21 @@
+import csv
+import os
+from pprint import pformat
+
+import logging
+log = logging.getLogger(__name__)
+
 import datetime
 from django.utils import timezone
+from datetime import datetime, timedelta
 
 from django.contrib.auth.models import User
 from opaque_keys.edx.keys import CourseKey
 from student.models import CourseEnrollment, UserProfile
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 from django.core.mail import send_mail, EmailMessage
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
 from sparta_pages.models import SpartaReEnrollment
 
@@ -18,62 +28,119 @@ class Command(BaseCommand):
         parser.add_argument(
             '-c', '--course',
             type=str,
-            required=True,
+            required=False,
             help='course ID to unenroll the user from'
             )
         parser.add_argument(
             '-u', '--user',
             type=str,
-            required=True,
+            required=False,
             help='Username for user'
         )
-
         parser.add_argument(
             '-m', '--mode',
             type=str,
             help='course enrollment mode to enroll user in'
+        )
+        parser.add_argument(
+            '--csv-file',
+            action='store',
+            dest='csv_file',
+            default=None,
+            help='Path of the file to read users from.',
+            type=str,
+            required=False
         )
 
     def handle(self, *args, **options):
         course_id = options.get('course', None)
         user = options.get('user', None)
         mode = options.get('mode', None)
+        csv_file = options.get('csv_file')
 
         if course_id is None:
             raise CommandError("Arguments course_id -c --course is required.")
 
-        if user is None:
-            raise CommandError("Arguments course_id -u --user is required.")
-
         try:
             course_key = CourseKey.from_string(course_id)
+            self.stdout.write("course_key: {}".format(course_key))
         except Exception as e:
             raise CommandError("Course does not exist: {}".format(str(e)))
 
-        tnow = timezone.now()
+        if user is not None:
+            try:
+                uname = User.objects.get(username=user)
+                reenrolled_user = self._reenroll_user(username=uname, course_key=course_key, mode=mode)
 
-        enrollments = CourseEnrollment.objects.filter(
-            course_id=course_key,
-            is_active=False,
-            user__username=user
-        )
+                msg = 'Successfully reenrolled user: {}.'.format(unenrolled_user)
+                log.info(msg)
+                self.stdout.write(msg)
 
-        if enrollments.exists() == False:
-            self.stdout.write("No enrollments found. course id: {}. user__username: {}".format(course_key,user))
+            except Exception as e:
+                raise CommandError("Error in appending user to list: {}".format(str(e)))
 
-        for e in enrollments:
-            if mode is None:
-                use_mode = e.mode
+        elif csv_file:
+            if not os.path.exists(csv_file):
+                raise CommandError(u'Pass the correct absolute path to file as --csv-file argument.')
+
+            total_users, failed_users = self._reenroll_users_from_file(csv_file, course_key=course_key, mode=mode)
+
+            if failed_users:
+                msg = u'Completed reenrolling the users. {} of {} failed.'.format(
+                    len(failed_users),
+                    total_users
+                )
+                log.error(msg)
+                self.stdout.write(msg)
+                msg = 'Failed users:{}'.format(pformat(failed_users))
+                log.error(msg)
+                self.stdout.write(msg)
             else:
-                use_mode = mode
+                msg = 'Successfully reenrolled {} users.'.format(total_users)
+                log.info(msg)
+                self.stdout.write(msg)
 
-            if e.is_active == False:
-                try:
-                    enrollment = CourseEnrollment.enroll(e.user, course_key, mode=use_mode, check_access=False)
-                    reenrollment = SpartaReEnrollment.objects.create(enrollment=enrollment,reenroll_date=tnow)
-                except Exception as e:
-                    raise CommandError("Error in reenrolling learner: {}".format(str(e)))
-                else:
-                    self.stdout.write(self.style.SUCCESS("Successfully reenrolled learner."))
-            else:
-                self.stdout.write(self.style.WARNING("User {} still has an active enrollment for this course.".format(e.user.username)))
+    def _reenroll_users_from_file(self, reenroll_file, course_key=None, mode=None):
+        """
+        Reenroll all the users provided in the users file.
+
+        file format, example file:
+            username
+            <string:username>
+
+        Arguments:
+            coupons_file (str): path of the file containing the list of users to be reenrolled.
+
+        Returns:
+            (total_users, failed_users): a tuple containing count of users processed and a list containing users that could not be processed.
+        """
+        failed_users= []
+
+        with open(reenroll_file, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                username=row['username']
+                result = self._unenroll_user(username=username, course_key=course_key, mode=mode)
+                if not result:
+                    failed_users.append(row)
+                    err_msg = u'Tried to process {}, but failed'
+                    log.error(err_msg.format(row))
+                line_count += 1
+        return line_count-1, failed_users
+
+    def _reenroll_user(self, username=None, course_key=None, mode=None):
+        """ reenroll a user """
+        try:
+            tnow = timezone.now()
+            uname = User.objects.get(username=username)
+            enrollment = CourseEnrollment.enroll(uname, course_key, mode=mode, check_access=False)
+            reenrollment = SpartaReEnrollment.objects.create(enrollment=enrollment,reenroll_date=tnow)
+        except Exception as e:
+            log.info("Error in reenrolling learner: {}".format(str(e)))
+            return False
+        else:
+            log.info("Successfully reenrolled learner: {}.".format(username))
+            return True
