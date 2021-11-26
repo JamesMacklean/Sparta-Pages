eca66c768a657acee5960e1e44af6e39663aa0f0
Engr. James Lusuegro <jamesmacklean27@gmail.com>
2021-11-26 14:00:35 +0800

    feat: organizational progress

---
+++ sparta_pages/management/commands/export_organizational_progress.py
@@ -0,0 +1,57 @@
+import csv
+import os
+from pprint import pformat
+from datetime import datetime, date, timedelta
+from django.utils import timezone
+
+import logging
+log = logging.getLogger(__name__)
+
+from django.core.management.base import BaseCommand, CommandError
+
+from sparta_pages.utils import export_organizational_progress
+
+class Command(BaseCommand):
+    help = 'Exports list of learners with their corresponding orgs and pathway progress.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-e',
+            '--email',
+            type=str,
+            help='set email to send to',
+        )
+        parser.add_argument(
+            '-f',
+            '--fromdate',
+            type=str,
+            help='set from date (format: Y-M-D)',
+        )
+        parser.add_argument(
+            '-t',
+            '--todate',
+            type=str,
+            help='set to date (format: Y-M-D)',
+        )
+
+    def handle(self, *args, **options):
+        email_address = options.get('email', None)
+        fromdate = options.get('fromdate', None)
+        todate = options.get('todate', None)
+
+        if fromdate is not None:
+            date_from = datetime.strptime(fromdate, "%Y-%m-%d")
+        else:
+            date_from = datetime.now().date() - timedelta(days=1)
+
+        if todate is not None:
+            date_to = datetime.strptime(todate, "%Y-%m-%d")
+        else:
+            date_to = datetime.now()
+
+        try:
+            export_organizational_progress(email_address=email_address, date_from=date_from, date_to=date_to)
+        except Exception as e:
+            raise CommandError("export_organizational_progress.ERROR: {}".format(str(e)))
+        else:
+            self.stdout.write(self.style.SUCCESS("Successfully exported SPARTA organizational progress."))
