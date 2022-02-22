0fe060f39d7de2c5e4bae011d589a7a124137251
Engr. James Lusuegro <jamesmacklean27@gmail.com>
2022-02-22 16:17:16 +0800

    API: Unenrolling a learner

--- sparta_pages/views/admin_views.py
+++ sparta_pages/views/admin_views.py
@@ -339,6 +339,30 @@ def admin_approve_unenrollment_view(request, username, course_key):
     if not request.user.is_staff:
         raise HttpResponse(status=500)
 
+    courseoverview = CourseOverview.get_from_id(course_key)
+    course_name = courseoverview.display_name
+
+
+    if username is not None:
+        uname = User.objects.get(username=username)
+        _unenroll_user(username=uname, email_address=uname.email, course_key=course_key,  course_name=course_name)
+    
+    def _unenroll_user(username=None, email_address=None, course_key=None, course_name=None):
+        """ unenroll a user """
+    
+        try:
+            uname = User.objects.get(username=username)
+            CourseEnrollment.unenroll(uname, course_key, skip_refund=True)
+            email = EmailMessage(
+                'Course Access Unenrollment',
+                'Your course access has expired due to failure to complete it in 6 months OR you have been inactive for 3 months. You are now unenrolled from {}.\n\nHow long can I complete a Project SPARTA course?\nUpon enrollment, you have 6 months to finish a SPARTA course. Failure to complete the course in 6 months and/or inactivity for 3 months will result in course access revocation.\n\nFill out the Learner Request form within five business days should you wish to reenroll in this course.\n\nLearner Request Form:\n https://forms.gle/nc9e9JwK287BSDxv5'.format(course_name),
+                'learn@coursebank.ph',
+                [email_address],
+            )
+            email.send()
+        except Exception as e:
+            return False
+        
     return redirect('sparta-admin-inactivity')
 
 @require_POST
