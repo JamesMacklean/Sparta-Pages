3d0760c818b3589aa5a908927558cd8d5d5e7a8f
Engr. James Lusuegro <jamesmacklean27@gmail.com>
2022-02-27 12:44:33 +0800

    API: Revert Attempt

--- sparta_pages/views/admin_views.py
+++ sparta_pages/views/admin_views.py
@@ -344,15 +344,19 @@ def admin_approve_unenrollment_view(request, username, course_key):
 
     def _unenroll_user(username=None, email_address=None, course_key=None, course_name=None):
         """ unenroll a user """
-        usname = User.objects.get(username=username)
-        CourseEnrollment.unenroll(usname, course_key, skip_refund=True)
-        email = EmailMessage(
-            'Course Access Unenrollment',
-            'Your course access has expired due to failure to complete it in 6 months OR you have been inactive for 3 months. You are now unenrolled from {}.\n\nHow long can I complete a Project SPARTA course?\nUpon enrollment, you have 6 months to finish a SPARTA course. Failure to complete the course in 6 months and/or inactivity for 3 months will result in course access revocation.\n\nFill out the Learner Request form within five business days should you wish to reenroll in this course.\n\nLearner Request Form:\n https://forms.gle/nc9e9JwK287BSDxv5'.format(course_name),
-            'learn@coursebank.ph',
-            [email_address],
-                    )
-        email.send()
+        try:
+            usname = User.objects.get(username=username)
+            CourseEnrollment.unenroll(usname, course_key, skip_refund=True)
+            email = EmailMessage(
+                'Course Access Unenrollment',
+                'Your course access has expired due to failure to complete it in 6 months OR you have been inactive for 3 months. You are now unenrolled from {}.\n\nHow long can I complete a Project SPARTA course?\nUpon enrollment, you have 6 months to finish a SPARTA course. Failure to complete the course in 6 months and/or inactivity for 3 months will result in course access revocation.\n\nFill out the Learner Request form within five business days should you wish to reenroll in this course.\n\nLearner Request Form:\n https://forms.gle/nc9e9JwK287BSDxv5'.format(course_name),
+                'learn@coursebank.ph',
+                [email_address],
+            )
+            email.send()
+
+        except Exception as e:
+            return False
 
     if username is not None:
         uname = User.objects.get(username=username)
