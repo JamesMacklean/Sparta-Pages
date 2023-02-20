085e414af6cf0c672ad80b656212682f192769f6
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-02-20 17:51:15 +0800

    reverted master's apps.py

--- sparta_pages/apps.py
+++ sparta_pages/apps.py
@@ -1,23 +1,4 @@
 from django.apps import AppConfig
-from edx_django_utils.plugins import PluginURLs, PluginSettings
-from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
-
 
 class SpartaPagesConfig(AppConfig):
     name = 'sparta_pages'
-
-plugin_app = {
-        PluginURLs.CONFIG: {
-            ProjectType.LMS: {
-                PluginURLs.NAMESPACE: 'sparta-pages',
-                PluginURLs.REGEX: '^sparta-pages/',
-                PluginURLs.RELATIVE_PATH: 'urls',
-            }
-        },
-        # PluginSettings.CONFIG: {
-        #     ProjectType.LMS: {
-        #         SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: 'settings.common'},
-        #         SettingsType.TEST: {PluginSettings.RELATIVE_PATH: 'settings.test'},
-        #     }
-        # }
-    }
